b2c88acaa857119404f7facf37d0416058f7ff7c
kayonelee <klee93@live.com>
2023-04-06 22:28:03 -0400

    submission

---
+++ day24assignment/rockpapers.js
@@ -0,0 +1,30 @@
+let hands = ["rock", "paper", "scissors"];       // --Defining hands array with the values
+
+function getHand() {    // ---defining getHand function
+    return hands[parseInt(Math.random()*10) % 3]; // ---returns-hand from the array using the given bracket info from assignment
+}
+
+// --Below are the two objects for the two players-name and getHand-----
+let player1 = {
+    name: "Sally",
+    getHand:getHand
+};
+
+let player2 = {
+    name: "Bob",
+    getHand:getHand
+};
+
+function playRound(player1, player2) {  // ---Defining the Function called playRound with two arguments
+    let Sallyhand = player1.getHand;        // --Argument for player1 Sally
+    let Bobhand = player2.getHand;          // --Argument for player2 Bob
+    let winner = null;
+
+    console.log(player1.name + Sallyhand);  // --Logging
+    console.log(player1.name + Sallyhand);  // --Logging
+
+    if (Sallyhand === Bobhand) {    // ----If Sally and Bob has the same hands...
+        console.log("It's a tie");  // ----log that it is a tie
+        return winner;
+
+      } else if (                   // ---else
--- day24assignment/simpletask.js
+++ day24assignment/simpletask.js
@@ -1,12 +1,24 @@
-let taskManager = (function() {
-    #tasks = [];                   // -------empty array the # sign makes it private
+let taskManager = (function () {
+    let tasks = [];                   // -------empty/private array
     
-function addTask(thisisadescription) {
-    tasks.push(thisisadescription)
-} 
+    function addTask(description) { 
+        tasks.push(description);
+     
+        return addTask;
+    };
 
-addTask("Zoom Meeting at 8am");
-addTask("Lecture at 10:30AM");
+    function displayTasks(){     // -Function to display all tasks-Array-----
+        for (index of tasks) {   // -Iterate For of Loop, Task Array
+            console.log(index);  // -Logging 
+        };
+    }
+        return { addTask: addTask, displayTasks:displayTasks };
+        
+})();
 
-console.log(tasks);
-  })();
+taskManager.addTask("Zoom Meeting at 8AM");
+taskManager.addTask("Lecture at 10:30AM");
+taskManager.addTask("Lunch at 12PM");
+taskManager.addTask("Standup at 1PM");
+
+taskManager.displayTasks();       // -Logging all display tasks
