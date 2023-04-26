db9d817c50d78ddcf914fb09bde13a9e1df09278
kayonelee <klee93@live.com>
2023-04-25 22:16:24 -0400

    jquery commit1

---
+++ day37assignment/jquery.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    
+    $('#submit').click(function(event){ //Create a function that will fire when the submit <button> is clicked
+        event.preventDefault();  // Prevent form submission
+    
+    // create variables for each of the form inputs
+    let nameInput = $('#name');
+    let emailInput = $('#email');
+    let phoneInput = $('#phone');
+    
+    //Utilize a for loop to iterate of the entire required array
+    let required =[nameInput,emailInput,phoneInput]
+    let hasWarnings = false;
+
+    for (let i = 0; i< required.length; i++){
+    $('label[for="' + $(required[i]).attr('id') + '"]').removeClass('warning')
+    if(required[i].val()===""){
+        $('#message').html("Please fill out the required fields").addClass('warning');  // populate the message p with the following text:, add the warning class to this message
+        $('label[for="' + $(required[i]).attr('id') + '"]').addClass('warning')
+        hasWarnings = true;
+    }
+
+    }
+   
+    if(!hasWarnings){
+        $("form").css({display: "none"});
+        $("h2").html("Thank you");
+        }
+        });
+    
+        })
